7b80799ca392544770bd892e06e79261709ce5d2
韩洋 <hanyang@chuchujie.com>
2018-07-25 22:13:47 +0800

    解决视频列表问题

--- pages/video/index.js
+++ pages/video/index.js
@@ -11,27 +11,27 @@ Page({
   data: {
     userInfo: {},
 
-    movieList: {
+    movieList: 
+      
       [{
-          "other": "",
-          "time": 1532519734589,
-          "title": "韩洋阚旭纪录片测试",
-          "vid": "e0354z3cqjp"
+          other: "",
+          time: 1532519734589,
+          title: "韩洋阚旭纪录片测试",
+          vid: "e0354z3cqjp"
         },
         {
-          "other": "",
-          "time": 1532519734589,
-          "title": "韩洋阚旭纪录片测试",
-          "vid": "e0354z3cqjp"
+          other: "",
+          time: 1532519734589,
+          title: "韩洋阚旭纪录片测试",
+          vid: "e0354z3cqjp"
         },
         {
-          "other": "",
-          "time": 1532519734589,
-          "title": "韩洋阚旭纪录片测试",
-          "vid": "e0354z3cqjp"
+          other: "",
+          time: 1532519734589,
+          title: "韩洋阚旭纪录片测试",
+          vid: "e0354z3cqjp"
         }
       ]
-    },
   },
 
   /**
