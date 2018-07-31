ce7e450403b739c482302abe1116be11f882a6d8
韩洋 <hanyang@chuchujie.com>
2018-07-31 16:35:39 +0800

    更新视频文件，增加url check

--- pages/video/index.js
+++ pages/video/index.js
@@ -11,25 +11,28 @@ Page({
   data: {
     userInfo: {},
 
-    movieList: 
-      
+    movieList:
+
       [{
           other: "",
           time: 1532519734589,
-          title: "韩洋阚旭纪录片测试",
-          vid: "e0354z3cqjp"
+          title: "海边",
+          vid: "e0354z3cqjp",
+          src: "http://pcgm4rcvg.bkt.clouddn.com/marry/video1532782634171.mp4"
         },
         {
           other: "",
           time: 1532519734589,
-          title: "韩洋阚旭纪录片测试",
-          vid: "e0354z3cqjp"
+          title: "绿地",
+          vid: "e0354z3cqjp",
+          src: "http://pcgm4rcvg.bkt.clouddn.com/marry/video1532871405425.mp4"
         },
         {
           other: "",
           time: 1532519734589,
-          title: "韩洋阚旭纪录片测试",
-          vid: "e0354z3cqjp"
+          title: "喷泉",
+          vid: "e0354z3cqjp",
+          src: "http://pcgm4rcvg.bkt.clouddn.com/marry/video1532963841560.mp4"
         }
       ]
   },
